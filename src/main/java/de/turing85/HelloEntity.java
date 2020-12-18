4b4fb51d27ca5f700dd19f813dbc5055ed5e694f
Marco Bungart <marco.bungart@consol.de>
2020-12-18 23:48:32 +0100

    code commit

---
+++ src/main/java/de/turing85/HelloEntity.java
@@ -0,0 +1,14 @@
+package de.turing85;
+
+public class HelloEntity {
+
+  private String hello;
+
+  public String getHello() {
+    return hello;
+  }
+
+  public void setHello(String hello) {
+    this.hello = hello;
+  }
+}
---
+++ src/main/java/de/turing85/HelloResource.java
@@ -0,0 +1,17 @@
+package de.turing85;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/hello")
+public class HelloResource {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response hello() {
+        return Response.ok(new HelloEntity()).build();
+    }
+}
